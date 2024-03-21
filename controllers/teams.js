6323fdf6ff471d0483df256013cb9ce8444fcca5
‘Ricky <rjvicenzi@gmail.com>
2024-03-21 16:54:21 -0400

    added All Teams to header

--- controllers/teams.js
+++ controllers/teams.js
@@ -1,9 +1,25 @@
+const Team = require('../models/team')
+
 module.exports = {
-    new: newTeam
+    new: newTeam,
+    create
 };
 
 function newTeam(req, res) {
     res.render('teams/new'), {
-        errorMsg:''
+        errorMsg: ''
     }
 }
+
+async function create(req, res) {
+    req.body.homeKitColors = req.body.homeKitColors.trim()
+    try {
+        await Team.create(req.body)
+        res.redirect('teams/new')
+    } catch (err) {
+        console.log(err)
+        res.render('teams/new', {
+            errorMsg: err
+        })
+    }
+}   
--- routes/teams.js
+++ routes/teams.js
@@ -4,6 +4,10 @@ var teamsCtrl = require('../controllers/teams')
 
 // all paths start with /teams
 
+// GET /teams/new
 router.get('/new', teamsCtrl.new)
+
+// POST /teams
+router.post('/', teamsCtrl.create)
 
 module.exports = router;
